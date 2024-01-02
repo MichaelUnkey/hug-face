94631fc14f6a9803bc689692b33ffc2d1a850b42
Mike Silva <michael@unkey.dev>
2024-01-02 08:33:22 -0500

    Added Unkey and protected route

---
+++ app/protected/route.ts
@@ -0,0 +1,13 @@
+import { NextRequestWithUnkeyContext, withUnkey } from "@unkey/nextjs";
+import { NextResponse } from "next/server";
+
+export const POST = withUnkey(async (req) => {
+  if (!req.unkey.valid) {
+    return new NextResponse("unauthorized", { status: 403 });
+  }
+
+  // Process the request here
+  // You have access to the verification response using `req.unkey`
+  console.log(req.unkey);
+  return new NextResponse("Your API key is valid!");
+});
